1b4781044d220a3b3b4ee3d95853919c000c121d
duongss <duongsscc@gmail.com>
2018-04-05 14:55:55 +0700

    Duongver3

--- app/src/main/java/com/example/admin/banthuocdx/Dichvu/listthuoc/ThuocFragment.java
+++ app/src/main/java/com/example/admin/banthuocdx/Dichvu/listthuoc/ThuocFragment.java
@@ -1,8 +1,6 @@
 package com.example.admin.banthuocdx.Dichvu.listthuoc;
 
 
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
@@ -13,6 +11,8 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -24,7 +24,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.ArrayList;
-
+import java.util.List;
 
 
 public class ThuocFragment extends Fragment {
@@ -32,10 +32,15 @@ public class ThuocFragment extends Fragment {
     private AdapterThuoc adapterThuoc;
     private CardView cardView;
     public ArrayList<thuoc> list;
+    public ArrayList<thuoc> list1;
+    AutoCompleteTextView autocom;
     Connection con;
     KetnoiData kc = new KetnoiData();
     TextView textView;
-Button button;
+    Button button;
+    thuoc th = null;
+    List listtk;
+
     public ThuocFragment() {
         // Required empty public constructor
     }
@@ -45,18 +50,31 @@ Button button;
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_thuoc, container, false);
         recyclerView = view.findViewById(R.id.listRethuoc);
-        cardView=view.findViewById(R.id.itemX);
+        cardView = view.findViewById(R.id.itemX);
+        autocom = view.findViewById(R.id.autoComple);
+
         Hienthilistthuoc();
+
         return view;
 
     }
 
     private void Cauhinhlist() {
-        adapterThuoc = new AdapterThuoc(getLayoutInflater(), list,getContext());
+        adapterThuoc = new AdapterThuoc(getLayoutInflater(), list, getContext());
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getActivity());
         linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
         recyclerView.setLayoutManager(linearLayoutManager);
         recyclerView.setAdapter(adapterThuoc);
+    }
+
+    private void AutoCom() {
+        ArrayList listAuto = new ArrayList();
+        for (thuoc c : list) {
+            listAuto.add(c.getTenThuoc());
+        }
+        ArrayAdapter arrayAdapter = new ArrayAdapter(getActivity(), android.R.layout.select_dialog_item, listAuto);
+        autocom.setThreshold(1);
+        autocom.setAdapter(arrayAdapter);
     }
 
     public void Hienthilistthuoc() {
@@ -72,11 +90,11 @@ Button button;
                     PreparedStatement prest = con.prepareStatement(sql);
                     ResultSet rs = prest.executeQuery();
                     while (rs.next()) {
-                        thuoc th = new thuoc(rs.getString("Tenthuoc"), rs.getFloat("Giatien"), rs.getInt("Soluong"), rs.getString("Mota"),rs.getString("Anhthuoclist"));
-
+                        th = new thuoc(rs.getString("Tenthuoc"), rs.getFloat("Giatien"), rs.getInt("Soluong"), rs.getString("Mota"), rs.getString("Anhthuoclist"));
                         list.add(th);
                         mIncomingHandler.sendEmptyMessage(0);
                     }
+
                     prest.close();
                     con.close();
                 } catch (Exception e) {
@@ -89,6 +107,7 @@ Button button;
     Handler mIncomingHandler = new Handler(new Handler.Callback() {
         public boolean handleMessage(Message msg) {
             Cauhinhlist();
+            AutoCom();
             return true;
         }
     });
