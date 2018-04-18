e95fb29f969a6327e9b293f5d0e38fd62fcc3a7d
duongss <duongsscc@gmail.com>
2018-04-18 20:45:57 +0700

    banthuoc-ss2

---
+++ app/src/main/java/com/example/admin/banthuocdx/Dichvu/listthuoc/Laytk.java
@@ -0,0 +1,18 @@
+package com.example.admin.banthuocdx.Dichvu.listthuoc;
+
+
+import com.example.admin.banthuocdx.Dichvu.dangnhapdangky.ActivityDangNhap;
+import com.example.admin.banthuocdx.Doituong.tkkhachhang;
+
+public class Laytk {
+   ActivityDangNhap dangNhap= new ActivityDangNhap();
+    com.example.admin.banthuocdx.Doituong.tkkhachhang tkkhachhang = new tkkhachhang();
+   public void laykey(String tk)
+   {
+
+       tkkhachhang tkkhachhang = new tkkhachhang(tk);
+
+
+
+   }
+}
